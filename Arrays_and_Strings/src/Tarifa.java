cd1a4a7bf302078659ae7556ec0f5df0c16749cf
Xavier Verdad <xcv0001@auburn.edu>
2018-11-01 17:11:24 -0500

    This is Tarifa updated.

--- Arrays_and_Strings/src/Tarifa.java
+++ Arrays_and_Strings/src/Tarifa.java
@@ -1,3 +1,19 @@
-public class Tarifa {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Tarifa {
+    public static void main(String[] args) {
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+
+        int monthlyMemory = scan.readLine();
+        int testCases = Integer.parseInt(scan.readLine());
+        int totalMemoryAvailable = 0;
+
+        for (int i = 0; i < testCases; i++) {
+            totalMemoryAvailable += monthlyMemory - Integer.parseInt(scan.next());
+
+        }
+        System.out.println(totalMemoryAvailable += monthlyMemory);
+
+        }
 }
